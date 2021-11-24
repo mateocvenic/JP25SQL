c031edb62dccc8d8909cc931b1303c444eee1953
mateocvenic <93664979+mateocvenic@users.noreply.github.com>
2021-11-24 05:00:48 -0800

    Izrada baze i dodavanje entiteta
    -castingagencija,plesac,ples,opisposla

---
+++ zavrsni/castingagencija.sql
@@ -0,0 +1,19 @@
+drop database if exists castingagencija;
+create database castingagencija character set utf8;
+use castingagencija;
+
+create table castingagencija(
+);
+
+
+create table plesac (
+
+);
+
+create table ples (
+    
+);
+
+create table opisposla (
+    
+);
