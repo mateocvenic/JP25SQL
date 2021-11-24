0f6991fc488d8055c39c73a515e03b540a7ecb98
mateocvenic <93664979+mateocvenic@users.noreply.github.com>
2021-11-24 05:07:25 -0800

    Dodavanje atributa
    -ubacivanje potrebnih atributa

--- zavrsni/castingagencija.sql
+++ zavrsni/castingagencija.sql
@@ -3,17 +3,34 @@ create database castingagencija character set utf8;
 use castingagencija;
 
 create table castingagencija(
+    sifra int not null primary key auto_increment,
+    plesac int,
+    ples int,
+    opisposla int
 );
 
 
 create table plesac (
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50),
+    iskustvo decimal(18.2),
+    video varchar(50)
 
 );
 
 create table ples (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    uparu boolean
     
 );
 
 create table opisposla (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    poslodavac varchar(50),
+    videoprimjer varchar(50),
+    placa decimal(18.2)
     
 );
